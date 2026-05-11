9ceec551ee75e4df3253e54d578b407f8d6e0544
chrysa <greau.anthony+chrysa@gmail.com>
2026-05-11 22:01:08 +0200

    fix(ci): exclude GitVersion.yml from yaml-sorter, fix eslint config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,12 +1,12 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import * as tsParser from '@typescript-eslint/parser';
 
 export default [
-  { ignores: ['dist', 'node_modules'] },
   js.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
@@ -26,17 +26,26 @@ export default [
         sourceType: 'module',
       },
     },
+    settings: {
+      react: { version: 'detect' },
+    },
     plugins: {
-      '@typescript-eslint': tsPlugin,
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      ...tsPlugin.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
+  },
+  {
+    ignores: ['dist', 'node_modules', '*.config.js'],
   },
 ];
